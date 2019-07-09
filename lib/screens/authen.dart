c59667657e4eb812434119a7dc9fb396a83aa59a
peepraya <peepraya@gmail.com>
2019-07-09 15:58:54 +0700

    Font Success

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Authen extends StatefulWidget {
   @override
   _AuthenState createState() => _AuthenState();
@@ -8,10 +7,43 @@ class Authen extends StatefulWidget {
 
 //ออกแบบหน้าจอ
 class _AuthenState extends State<Authen> {
+  // Explicit ประกาศ Varible
+
+  // Method การเอา Statement มารวมกัน
+  Widget showLogo() {
+    // ดึงรูปภาพ
+    return Container(
+      // กำหนดความกว้างความสูงของรูปภาพ
+      width: 150.0,
+      height: 150.0, child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showText() {
+    //การประกาศ Method
+    return Text(
+      'PeeZa',
+      // กำหนด Font
+      style: TextStyle(
+          fontSize: 30.0, // กำหนดขนาด
+          color: Colors.orange[800], // กำหนดสี Font
+          fontWeight: FontWeight.bold, // กำหนดความหนา
+          fontFamily: 'OpenSans'), // กำหนด Font Family
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    
     //Tool สำหรับการออกแบบ
-    return Scaffold(body: Text('Authen'),);
+    return Scaffold(
+      // Container กำหนดตำแหน่งและขนาด
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0), //กำหนดความห่างจาก Method
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[showLogo(), showText()],
+        ), //child,children คำสั่งเรียกใช้ Method
+      ),
+    );
   }
 }
