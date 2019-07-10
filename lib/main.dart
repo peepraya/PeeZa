1453a8069dc95544a78cb6af50ae50c27113cce5
peepraya <peepraya@gmail.com>
2019-07-10 10:14:02 +0700

    TextFormField Success

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart'; //1
 import 'package:pee_tpa/screens/authen.dart';
 
+//ตั้งค่าจอ
+import 'package:flutter/services.dart';
+
 main() {
   runApp(MyApp());
-}  //2
+} //2
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        // กำหนด App ให้เป็นแนวตั้ง
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+        // กำหนด App ให้เป็นแนวนอน
+        // [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
     return MaterialApp(
       home: Authen(),
     );
   }
-}  //3
+} //3
--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -32,16 +32,48 @@ class _AuthenState extends State<Authen> {
     );
   }
 
+  Widget emailText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        // กำหนดประเภทของคีย์บอร์ด
+        keyboardType: TextInputType.emailAddress,
+        //สำหรับให้กรอก
+        decoration:
+            InputDecoration(labelText: 'Email :', hintText: 'you@email.com'),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true, //เปิดใช้ . แทน Text
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          hintText: 'More 6 Charactor',
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     //Tool สำหรับการออกแบบ
     return Scaffold(
+      resizeToAvoidBottomPadding: false, //อนุญาติให้คีย์บอร์ดทับ Widget
       // Container กำหนดตำแหน่งและขนาด
       body: Container(
         padding: EdgeInsets.only(top: 60.0), //กำหนดความห่างจาก Method
         alignment: Alignment.topCenter,
         child: Column(
-          children: <Widget>[showLogo(), showText()],
+          children: <Widget>[
+            showLogo(),
+            showText(),
+            emailText(),
+            passwordText()
+          ],
         ), //child,children คำสั่งเรียกใช้ Method
       ),
     );
